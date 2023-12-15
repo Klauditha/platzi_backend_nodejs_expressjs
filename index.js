079aec2793c70de46fabc76cfaf74a5b43821df4
Claudia Villarroel <claudia.villarroel@live.com>
2023-12-15 09:11:31 -0300

    :hammer: Instalacion express y servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("My app");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+  res.send('Hola mi server en express');
+});
+
+
+app.listen(port, () => {
+  console.log('Mi port: ' + port);
+});
