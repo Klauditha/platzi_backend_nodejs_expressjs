ddd1abd32b46a4f485dabf772a64177f7d409f13
Claudia Villarroel <claudia.villarroel@live.com>
2023-12-15 10:00:09 -0300

    :hammer: Get recibir parametros

--- index.js
+++ index.js
@@ -2,18 +2,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta',(req,res) => {
+app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products',(req,res) => {
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 100,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 100
+    id,
+    name: 'Product ' + req.params.id,
+    price: 100,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
