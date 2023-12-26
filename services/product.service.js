24bbe357a80240ae1d31f87a91ec5fab136700df
Claudia Villarroel <claudia.villarroel@live.com>
2023-12-26 13:38:40 -0300

    :hammer: Async await y captura de errores

--- services/product.service.js
+++ services/product.service.js
@@ -17,7 +17,7 @@ class ProductsService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data,
@@ -26,13 +26,19 @@ class ProductsService {
     return newProduct;
   }
   find() {
-    return this.products;
-  }
-  findOne(id) {
+    return new Promise((resolve,reject) =>{
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    }
+    //return this.products;
+    )};
+
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
@@ -45,7 +51,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
